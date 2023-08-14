22db006ed01f7b29df8f00090f10b85ca9a430ed
Levi Oister <loister72@outlook.com>
2023-08-14 14:47:16 -0500

    Edited config to be matching with Variables files

--- Terraform Modules/Windows-VM/main.tf
+++ Terraform Modules/Windows-VM/main.tf
@@ -1,22 +1,56 @@
-# Terraform module to provision a virtual machine on VMware
-
-# Import the VMware provider
 provider "vsphere" {
-    user     = "myuser"
-    password = "mypassword"
-    vsphere_server = "vcenter.example.com"
+  user           = "${var.vsphere_user}"
+  password       = "${var.vsphere_password}"
+  vsphere_server = "${var.vsphere_server}"
+  allow_unverified_ssl = true
 }
 
-# Create a virtual machine
-resource "vsphere_virtual_machine" "example" {
-    name             = "example-vm"
-    resource_pool_id = "resource-pool-id"
-    datastore_id     = "datastore-id"
-    folder           = "vm-folder"
-    num_cpus         = 2
-    memory           = 4096
-    guest_id         = "ubuntu64Guest"
-    network_interface {
-        network_id = "network-id"
-    }
+data "vsphere_datacenter" "dc" {
+  name = "${var.vsphere_datacenter}"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "${var.vsphere_datastore}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "${var.vsphere_resource_pool}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "${var.vsphere_network}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "${var.vsphere_virtual_machine_template}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_virtual_machine" "cloned_virtual_machine" {
+  name             = "${var.vsphere_virtual_machine_name}"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+
+  num_cpus = "${data.vsphere_virtual_machine.template.num_cpus}"
+  memory   = "${data.vsphere_virtual_machine.template.memory}"
+  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+
+  scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
+
+  network_interface {
+    network_id   = "${data.vsphere_network.network.id}"
+    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
+  }
+
+  disk {
+    label = "disk0"
+    size = "${data.vsphere_virtual_machine.template.disks.0.size}"
+  }
+
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+  }
 }
