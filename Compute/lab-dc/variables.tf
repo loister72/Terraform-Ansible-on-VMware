d6d2e5fd9c962145ce862052c32fde5df25d6b67
Levi Oister <loister72@outlook.com>
2023-09-06 12:19:00 -0500

    Variables oops

---
+++ Compute/lab-dc/variables.tf
@@ -0,0 +1,12 @@
+# vsphere login account. defaults to admin account
+variable "vsphere_user" {
+  default = "administrator@vsphere.local"
+}
+
+# vsphere account password. empty by default.
+variable "vsphere_password" {}
+
+# vsphere server, defaults to localhost
+variable "vsphere_server" {
+  default = "localhost"
+}
