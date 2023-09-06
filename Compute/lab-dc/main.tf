3b73cf2c3b89c348d948954db02076622317998b
Levi Oister <loister72@outlook.com>
2023-09-06 11:16:54 -0500

    New Config

--- Compute/lab-dc/main.tf
+++ Compute/lab-dc/main.tf
@@ -1,58 +1,36 @@
-provider "vsphere" {
-  user                 = var.vsphere_user
-  password             = var.vsphere_password
-  vsphere_server       = var.vsphere_server
-  allow_unverified_ssl = true
+data "vsphere_datacenter" "dc" {
+  name = "${var.vsphere_datacenter}"
+}
 
-data "vsphere_datacenter" "datacenter" {
-  name = "dc-01"
+data "vsphere_datastore_cluster" "datastore_cluster" {
+  count         = var.vsphere_datastore_cluster != "" ? 1 : 0
+  name          = var.vsphere_datastore_cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "DevLab"
-  datacenter_id = data.vsphere_datacenter.datacenter.id
+  count         = var.vsphere_datastore != "" && var.vsphere_datastore_cluster == "" ? 1 : 0
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_compute_cluster" "cluster" {
-  name          = "devlab-cl01"
-  datacenter_id = data.vsphere_datacenter.datacenter.id
+data "vsphere_resource_pool" "pool" {
+  name          = var.vsphere_resource_pool
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_network" "network" {
-  name          = "pdg-servers"
-  datacenter_id = data.vsphere_datacenter.datacenter.id
+  count         = var.network_cards != null ? length(var.network_cards) : 0
+  name          = var.network_cards[count.index]
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "win-server-22-tmplt"
-  datacenter_id = data.vsphere_datacenter.datacenter.id
+  name          = "${var.vsphere_vm_template_name}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-resource "vsphere_virtual_machine" "vm" {
-  name             = "lab-dc"
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  datastore_id     = data.vsphere_datastore.datastore.id
-  num_cpus         = 2
-  memory           = 8192
-  guest_id         = data.vsphere_virtual_machine.template.guest_id
-  scsi_type        = data.vsphere_virtual_machine.template.scsi_type
-  network_interface {
-    network_id   = data.vsphere_network.network.id
-    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
-  }
-  disk {
-    label            = "disk0"
-    size             = data.vsphere_virtual_machine.template.disks.0.size
-    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
-  }
-  clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
-    customize {
-      network_interface {
-        ipv4_address = "10.100.40.10"
-        ipv4_netmask = 24
-      }
-      ipv4_gateway = "10.100.40.1"
-    }
-  }
+locals {
+  interface_count     = length(var.ipv4_submask) #Used for Subnet handeling
+  template_disk_count = length(data.vsphere_virtual_machine.template.disks)
 }
--- Compute/lab-dc/terraform.tfvars
+++ Compute/lab-dc/terraform.tfvars
@@ -1,9 +1,17 @@
-vsphere_user = "terraform_user@vsphere.local"
-vsphere_password = "kvc1DQM_trg_cjt2xjm"
-vsphere_server = "vcenter.oisterfam.net"
-vsphere_datacenter = "DevLab"
-vsphere_datastore = "esxi02-ds"
-vsphere_resource_pool = "/DevLab/host/devlab-cl01/Resources/lab-rp"
-vsphere_network = "pdg-servers"
-vsphere_virtual_machine_template = "win-server-22-tmplt"
-vsphere_virtual_machine_name = "lab-dc"
+vsphere_datacenter                  = "DevLab"
+vsphere_datastore                   = "esxi02-ds"
+vsphere_resource_pool               = "/DevLab/host/devlab-cl01/Resources/lab-rp"  // If no Resource Pool is present, use vSphere Cluster
+network_cards                       = ["pdg-servers"] // vSwitch/DVS Port Group
+vsphere_vm_template_name            = "win-server-22-tmplt"
+vsphere_virtual_machine_name        = "lab-dc"
+vsphere_virtual_machine_count       = 1
+vsphere_virtual_machine_cpu_count   = "2"
+vsphere_virtual_machine_memory_size = "8192" //Megabytes
+cpu_hot_add_enabled                 = true
+cpu_hot_remove_enabled              = true
+memory_hot_add_enabled              = true
+domain                              = "pdglab.local"
+vm_gateway                          = "10.100.40.1" // Your gateway address
+vm_dns = [                                       // Your DNS server address
+  "10.100.40.1",
+]
