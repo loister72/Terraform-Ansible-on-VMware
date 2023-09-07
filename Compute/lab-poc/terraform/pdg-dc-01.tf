51669c2961d1a4c9b67ac4eaf818b8e6e94e9bb6
Levi Oister <loister72@outlook.com>
2023-09-06 20:57:38 -0500

    bad command from exmaple

--- Compute/lab-poc/terraform/pdg-dc-01.tf
+++ Compute/lab-poc/terraform/pdg-dc-01.tf
@@ -35,7 +35,7 @@ resource "vsphere_virtual_machine" "pdg-dc-01" {
         auto_logon_count = 1
 
         # Run these commands after autologon. Configure WinRM access and disable windows firewall.
-        run_once_command_list = [
+        run_once = [
           "winrm quickconfig",
           "winrm set winrm/config @{MaxEnvelopeSizekb=\"100000\"}",
           "winrm set winrm/config/Service @{AllowUnencrypted=\"true\"}",
